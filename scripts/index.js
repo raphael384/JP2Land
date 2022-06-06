2da377094de9f52b27bc122c6d17a88319ccaeee
raphael384 <abazerty384@gmail.com>
2022-06-06 18:49:54 +0200

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,68 @@
+
+var question;
+var loirat;
+var martineau;
+var troadec;
+var charette;
+
+var loiratPoints;
+var martineauPoints;
+var troadecPoints;
+var charettePoints;
+
+
+
+
+var currentText = 0
+
+function load() {
+
+    question = document.getElementById("question")
+    loirat = document.getElementById("loirat")
+    martineau = document.getElementById("martineau")
+    troadec = document.getElementById("troadec")
+    charette = document.getElementById("charette")
+
+    loiratPoints = 0;
+    martineauPoints = 0;
+    troadecPoints = 0;
+    charettePoints = 0;
+
+}
+
+function nextText() {
+    currentText ++;
+
+    if (currentText >= questionsTexts.length) {
+        question.textContent = "Tu es :"
+        loirat.textContent = "Mr Loirat à " + loiratPoints*100/loiratTexts.length + "%"
+        martineau.textContent = "Mr Martineau à " + martineauPoints*100/loiratTexts.length + "%"
+        troadec.textContent = "Mme Troadec à " + troadecPoints*100/loiratTexts.length + "%"
+        charette.textContent = "Mme de Charrete à " + charettePoints*100/loiratTexts.length + "%"
+        loirat = null
+        martineau = null
+        troadec = null
+        charette = null
+    } else {
+        question.textContent = questionsTexts[currentText]
+        loirat.textContent = loiratTexts[currentText]
+        martineau.textContent = martineauTexts[currentText]
+        troadec.textContent = troadecTexts[currentText]
+        charette.textContent = charetteTexts[currentText]
+    }
+
+    
+
+}
+
+
+load()
+        
+
+
+loirat.onclick = function() {loiratPoints ++; nextText()}
+martineau.onclick = function() {martineauPoints ++; nextText()}
+troadec.onclick = function() {troadecPoints ++; nextText()}
+charette.onclick = function() {charettePoints ++; nextText()}
+
+
---
+++ scripts/texts.js
@@ -0,0 +1,62 @@
+const questionsTexts = [
+    "C'est le premier jour de cours !",
+    "C'est le chahut dans la classe ...",
+    "Un élève ne comprend pas une notion ...",
+    "C'est le premier avril, tu trouves une punaise sur ta chaise ...",
+    "Tu rêverais d'avoir des élèves ...",
+    "Ton cauchemar ...",
+    "Ton metier si tu n'etais pas prof : ",
+    "La fin de l'année approche",
+    "L'alarme retentit en plein cours, c'est un exercice :",
+    "Tu surprends des élèves qui s'embrassent ..."
+
+]
+const loiratTexts = [
+    "Il va falloir rester vigilant et suivre le règlement a la lettre.",
+    "Pas besoin d'élever la voix, un regard sombre suffit",
+    "Tu le prends en particulier, il risque de ne pas avoir son brevet",
+    "\"Mais quel manque de respect dans une école catholique !!\"",
+    "Très disciplinés.",
+    "Des élevès rebelles et délinquants.",
+    "Boulanger.",
+    "On lâche rien, on est là pour travailler jusqu'au bout !",
+    "Tu expliques sans aucun calme qu'il faut rester calme.",
+    "S'ensuit une copieuse distribution d'heures de colles, d'humiliations publiques, d'homélies sur les relations affectives, etc...",
+]
+const martineauTexts = [
+    "Et merde, il faut retourner bosser ...",
+    "\"Me faites pas chier, on est lundi !!\"",
+    "Tu expliques rapidement, pas de temps à perdre, on n'a pas que ça à foutre.",
+    "\"C'est quoi ça ??\", soupires-tu",
+    "Sympas mais calmes",
+    "Moins de congés",
+    "Militaire",
+    "Tu glandes déjà depuis un mois",
+    "\"Allez, on se lève et on la ferme...\"",
+    "Tu éclates de rire"
+]
+const troadecTexts = [
+    "Je vais découvrir mes nouvelles classes, pourvu qu'ils soient gentils.",
+    "Tu fuis, il n'y a rien à faire.",
+    "Tu répètes encore et encore ce qu'il y a écrit dans la leçon.",
+    "Tu quittes définitivement cet établissement horrible.",
+    "Beaucoup plus jeunes et gentils",
+    "Tu le vis déjà !",
+    "Jardinier",
+    "Je vais enfin retrouver ma liberté !",
+    "C'est quoi ?",
+    "Choqué, tu passes ton chemin"
+]
+const charetteTexts = [
+    "Tout est préparé, on commence par 6 mois de revisions.",
+    "\"Taisez-vous ! Ou je mets un exercice noté !\"",
+    "Hélas, tu est définitivement un prof incompris.",
+    "\"Vous avez la mentalité d'élevès de primaire !\"",
+    "Passionés par la matière que tu enseignes.",
+    "La matière que tu enseignes disparaît",
+    "Moine, bonne soeur.",
+    "Nous allons enfin commencer le programme de cette année",
+    "<soupir> toujours au mauvais moment ...",
+    "Outré, tu informes tes collègues qui emploieront la première proposition."
+
+]
